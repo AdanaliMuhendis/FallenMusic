5265dc4fcb6a1e9aa3b40c1478fe888e6b108e0e
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-07-01 01:25:08 +0300

    Update activevc.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- FallenMusic/Modules/activevc.py
+++ FallenMusic/Modules/activevc.py
@@ -30,7 +30,7 @@ from FallenMusic.Helpers.inline import close_key
 
 @app.on_message(filters.command("activevc") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("» Sesli Sohbetlerin Listesi Alınıyor...")
     chats = await get_active_chats()
     text = ""
     j = 0
@@ -46,10 +46,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{chat}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("Aktif Sesli Sohbet Bulunamadı...")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"<b>**Alem Müzik Aktif Sesli Sohbet Listesi :**\n\n{text}</b>",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
