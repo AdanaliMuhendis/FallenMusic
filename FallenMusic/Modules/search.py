d0401d2497592efe2155cc88362ec360e2dbab7b
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-07-01 02:27:00 +0300

    Update search.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -27,7 +27,7 @@ from youtube_search import YoutubeSearch
 from FallenMusic import app
 
 
-@app.on_message(filters.command(["search", "bul", "indir", "song"]))
+@app.on_message(filters.command(["search"]))
 async def ytsearch(_, message: Message):
     try:
         await message.delete()
