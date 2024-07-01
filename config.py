b863befadc471e8072a389296d4c86dac098b291
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-07-01 19:20:03 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/Alem-Music-05-27")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/Alem-Music-05-27")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/430-490-07-01")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/430-490-07-01")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alemciyiz")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5988763828").split()))
 
 
-FAILED = "https://te.legra.ph/Alem-Music-05-27"
+FAILED = "https://te.legra.ph/430-490-07-01"
